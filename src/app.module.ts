8a6dac6be98e3a8da4c3a232bd67dfaa1634cd14
Ali Amini <aliamini.af@gmail.com>
2024-04-16 07:36:41 +0330

    feat(user): add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule],
   controllers: [PostsController],
   providers: [
     AppService,
