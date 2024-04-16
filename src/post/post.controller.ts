ddaa660b2dbed98192bea9242f2778833af752fa
Ali Amini <aliamini.af@gmail.com>
2024-04-16 10:17:23 +0330

    feat(post): implement get all posts endpoint

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { PostResponseDto } from './dto/post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
+  constructor(private readonly postService: PostService) {}
+
   @Get() 
-  getAllPost(): string {
-    return 'All Post';
+  async getAllPost(): Promise<PostResponseDto[]> {
+    return this.postService.getAllPosts();
   }
 
   @Post()
---
+++ src/post/post.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PostResponseDto } from './dto/post.dto';
+
+@Injectable()
+export class PostService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getAllPosts(): Promise<PostResponseDto[]> {
+    const posts = await this.prismaService.post.findMany();
+    return posts;
+  }
+}
