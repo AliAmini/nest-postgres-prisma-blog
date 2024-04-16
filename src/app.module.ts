2f12ad607750824b1f8a1acb9a5cc06107898568
Ali Amini <aliamini.af@gmail.com>
2024-04-16 10:16:59 +0330

    fix(module): fix modules issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,18 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppService } from './app.service';
 import { PostController } from './post/post.controller';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PostService } from './post/post.service';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
-  controllers: [PostController],
+  imports: [UserModule, PostModule],
+  controllers: [],
   providers: [
     AppService,
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
-    },
+    }
   ],
 })
 export class AppModule {}
