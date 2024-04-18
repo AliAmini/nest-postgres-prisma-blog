c6534e47b633abd53af51a29c4e22daa52a191f5
Ali Amini <aliamini.af@gmail.com>
2024-04-18 08:57:26 +0330

    feat(comment): implement create comment endpoint

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Body, Param } from '@nestjs/common/decorators';
+import { ParseIntPipe } from '@nestjs/common/pipes';
+import { User, UserInfo } from 'src/user/decorators/user.decorator';
+import { CommentService } from './comment.service';
+import { CommentResponseDto, CreateCommentDto } from './dtos/comment.dto';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post('post/:postId')
+  createComment(
+    @Body() {content}: CreateCommentDto,
+    @Param('postId', ParseIntPipe) postId: number,
+    @User() user: UserInfo
+  ) {
+    return this.commentService.createComment(user.id, postId, content);
+  }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CommentResponseDto, CreateCommentDto } from './dtos/comment.dto';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createComment(userId: number, postId: number, commentContent: string): Promise<CommentResponseDto> {
+    const comment = await this.prismaService.comment.create({
+      data: {
+        content: commentContent,
+        user_id: userId,
+        post_id: postId
+      }
+    });
+
+    return new CommentResponseDto(comment);
+  }
+}
