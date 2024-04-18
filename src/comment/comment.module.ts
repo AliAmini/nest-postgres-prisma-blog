c53f74d5c8b82de462769ded1cd706212aee7bfa
Ali Amini <aliamini.af@gmail.com>
2024-04-18 09:38:02 +0330

    feat(comment): send email for comments

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EmailModule } from 'src/email/email.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EmailModule],
   controllers: [CommentController],
   providers: [CommentService]
 })
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { EmailService } from 'src/email/email.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CommentResponseDto, CreateCommentDto } from './dto/comment.dto';
 
 @Injectable()
 export class CommentService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly emailService: EmailService
+  ) {}
 
   async getComments(postId: number): Promise<CommentResponseDto[]> {
     const comments = await this.prismaService.comment.findMany({
@@ -13,10 +17,12 @@ export class CommentService {
         user: true,
       },
     });
+    
     return comments.map((comment) => new CommentResponseDto(comment));
   }
 
   async createComment(userId: number, postId: number, commentContent: string): Promise<CommentResponseDto> {
+    // create comment
     const comment = await this.prismaService.comment.create({
       data: {
         content: commentContent,
@@ -25,6 +31,26 @@ export class CommentService {
       }
     });
 
+    // send email notification to user
+    await this.sendEmailNotification(postId);
+
     return new CommentResponseDto(comment);
+  }
+
+  private async sendEmailNotification(postId: number) {
+    const post = await this.prismaService.post.findUnique({
+      where: {id: postId},
+      select: {
+        title: true,
+        user: true
+      }
+    });
+
+    // send email notification to queue
+    await this.emailService.sendCommentEmail({
+      postTitle: post.title,
+      postCreatorEmail: post.user.email,
+      postCreatorName: post.user.name
+    });
   }
 }
